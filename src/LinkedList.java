9179e00844ebbf1e702ecca26def7068cce10bbe
aqd14 <aqd14@msstate.edu>
2018-03-08 21:04:15 -0600

    Override equals

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -49,4 +49,24 @@ public class LinkedList {
         }
         return bd.toString();
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == null || other == null) {
+            return false;
+        }
+
+        Node temp1 = this.head;
+        LinkedList otherLK = (LinkedList) other;
+        Node temp2 = otherLK.head;
+
+        while (temp1 != null || temp2 != null) {
+            if (temp1.item != temp2.item) return false;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        if (temp1 == null && temp2 == null) return true;
+        return false;
+    }
 }
